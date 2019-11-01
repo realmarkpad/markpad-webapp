6d19c2b717aeb70426e9b390496548eb6ef652ab
Rafael Coelho <rafaellcoellho@gmail.com>
2019-11-01 17:59:16 -0300

    put logo on Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="container">
     <div class="search">
-      <h1 class="title">Markpad</h1>
-      <input
-        v-model="document"
-        type="text"
-        placeholder="Access your document"
-        @keydown.enter="goToDocument"
-      />
-      <button @click="goToDocument">Search</button>
+      <img class="logo" src="../assets/logo.jpg" />
+      <div class="input">
+        <input
+          class="search-input"
+          v-model="document"
+          type="text"
+          placeholder="Access your document"
+          @keydown.enter="goToDocument"
+        />
+        <a class="search-button" @click="goToDocument">
+          <img
+            class="search-icon"
+            src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"
+          />
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -43,18 +51,62 @@ export default {
 .container {
   min-height: 100vh;
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(3, 1fr);
-  justify-items: center;
-  align-items: center;
+  grid-template-columns: 0.2fr 1fr 0.2fr;
+  grid-template-rows: 0.2fr 1fr 0.2fr;
 
   .search {
-    grid-row: 2/2;
-    grid-column: 2/2;
+    grid-row: 2/3;
+    grid-column: 2/3;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
-    .title {
-      text-align: center;
-      margin: 30px 10px;
+    .logo {
+      margin: 0 auto;
+      width: 820px;
+      height: 312px;
+    }
+    .input {
+      margin: 0 auto;
+      display: inline;
+
+      .search-input {
+        margin: 0 auto;
+        width: 100%;
+        height: 45px;
+        padding: 0 20px;
+        font-size: 1rem;
+        border: 1px solid #d0cfce;
+        outline: none;
+
+        &:focus {
+          border: 1px solid #008abf;
+          transition: 0.35s ease;
+          color: #008abf;
+
+          &::-webkit-input-placeholder {
+            transition: opacity 0.45s ease;
+            opacity: 0;
+          }
+          &::-moz-placeholder {
+            transition: opacity 0.45s ease;
+            opacity: 0;
+          }
+          &:-ms-placeholder {
+            transition: opacity 0.45s ease;
+            opacity: 0;
+          }
+        }
+      }
+      .search-icon {
+        position: relative;
+        float: right;
+        width: 75px;
+        height: 75px;
+        top: -62px;
+        right: -45px;
+      }
     }
   }
 }
